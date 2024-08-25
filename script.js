f3f547525118d02fbeed0d0e89ae0afb54841be5
Filippo <tomainofilippo@gmail.com>
2024-08-25 22:25:12 +0200

    Created regular size divs
    Figured out how to find the right width and height
    for all the divs: finding the width of the
    container with container.offsetWidth function and
    dividing it by the times you want to create the
    divs.

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
 function createDivs(times) {
-    for(let i = 0; i < times; i++) {
-        const div = document.createElement("div");
-
-        div.style.height = "170px";
-        div.style.width = "170px";
-        div.style.background = "red";
-        
-        container.appendChild(div);
-
-        div.addEventListener("mouseover", () => {
-            div.style.background = "green";
-        });
-
-        div.addEventListener("mouseout", () => {
+    for(let k = 0; k < times; k++) {
+        for(let i = 0; i < times; i++) {
+            const div = document.createElement("div");
+    
+            div.style.height = `${Math.floor(container.offsetWidth / times)}px`;
+            div.style.width = `${Math.floor(container.offsetWidth / times)}px`;
             div.style.background = "red";
-        });
-    }
-}
 
-createDivs(16);
+            console.log(container.offsetWidth / times)
+            
+            container.appendChild(div);
+    
+            div.addEventListener("mouseover", () => {
+                div.style.background = "green";
+            });
+    
+            div.addEventListener("mouseout", () => {
+                div.style.background = "red";
+            });
+        }
+    }
+};
+
+function removeDivs(times) {
+    for(let i = 0; i < times; i++) {
+        container.remove();
+    }
+};
+
+button.addEventListener("click", () => {
+    let sides = prompt("Size of the sketch? (MAX 100)");
+    createDivs(sides);
+    removeDivs(sides);
+});
+
